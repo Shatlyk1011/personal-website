26039f015c51605697fff348e5b44b0d71b49524
Shatlyk Abdullayev <gj_wp@mail.ru>
2025-07-03 13:50:51 +0500

    install motion add aurora bg

---
+++ src/components/ui/AuroraBg/index.tsx
@@ -0,0 +1,82 @@
+"use client";
+import { cn } from "@/shared/utils";
+import React, { ReactNode } from "react";
+
+interface AuroraBackgroundProps extends React.HTMLProps<HTMLDivElement> {
+  children: ReactNode;
+  showRadialGradient?: boolean;
+}
+
+export const AuroraBackground = ({
+  className,
+  children,
+  showRadialGradient = false,
+  ...props
+}: AuroraBackgroundProps) => {
+  return (
+    <div
+      className={cn(
+        "transition-bg relative flex h-[100vh] w-full flex-col items-center justify-center bg-zinc-50 text-slate-950",
+        className,
+      )}
+      {...props}
+    >
+      <div
+        className="absolute inset-0 overflow-hidden"
+        style={
+          {
+            "--aurora":
+              "repeating-linear-gradient(100deg,#3b82f6_10%,#a5b4fc_15%,#93c5fd_20%,#ddd6fe_25%,#60a5fa_30%)",
+            "--white-gradient":
+              "repeating-linear-gradient(100deg,#fff_0%,#fff_7%,transparent_10%,transparent_12%,#fff_16%)",
+
+            // "--blue-300": "#93c5fd",
+            // "--blue-400": "#60a5fa",
+            // "--blue-500": "#3b82f6",
+            // "--indigo-300": "#a5b4fc",
+            // "--violet-200": "#ddd6fe",
+
+            "--blue-300": "#052655",
+            "--blue-400": "#052655",
+            "--blue-500": "#070707",
+            "--indigo-300": "#070707",
+            "--violet-200": "#fff",
+
+            // "--blue-300": "#070707",
+
+            // "--blue-400": "#393A39",
+
+            // "--blue-500": "#DCDCDC",
+
+            // "--indigo-300": "#A5A5A5",
+
+            // "--violet-200": "#787878",
+
+            
+            // "--blue-300": "#2ff",
+            // "--blue-400": "#f00",
+            // "--blue-500": "#ff0",
+            // "--indigo-300": "#f0f",
+            // "--violet-200": "#00f",
+            "--black": "#000",
+            "--white": "#fff",
+            "--transparent": "transparent",
+          } as React.CSSProperties
+        }
+      >
+        <div
+          //   I'm sorry but this is what peak developer performance looks like // trigger warning
+          className={cn(
+            `after:animate-aurora pointer-events-none absolute -inset-[10px] [background-image:var(--white-gradient),var(--aurora)] [background-size:300%,_200%] [background-position:-50%_-50%,-50%_-50%] opacity-10 blur-[10px] invert filter will-change-transform [--aurora:repeating-linear-gradient(-100deg,var(--blue-500)_20%,var(--indigo-300)_15%,var(--blue-300)_20%,var(--violet-200)_25%,var(--blue-400)_30%)] [--dark-gradient:repeating-linear-gradient(-100deg,var(--black)_0%,var(--black)_7%,var(--transparent)_10%,var(--transparent)_12%,var(--black)_16%)] [--white-gradient:repeating-linear-gradient(-100deg,var(--white)_0%,var(--white)_7%,var(--transparent)_10%,var(--transparent)_12%,var(--white)_16%)] after:absolute after:inset-0 after:[background-image:var(--white-gradient),var(--aurora)] after:[background-size:200%,_100%] after:[background-attachment:fixed] after:mix-blend-difference after:content-[""]`,
+
+            showRadialGradient &&
+              `[mask-image:radial-gradient(ellipse_at_100%_0%,black_10%,var(--transparent)_70%)]`,
+          )}
+
+         
+        ></div>
+      </div>
+      {children}
+    </div>
+  );
+};
