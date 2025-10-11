d127c6e6fee70f278fae5760362bdb91c1b4d6e8
Shatlyk Abdullayev <gj_wp@mail.ru>
2025-10-11 21:58:23 +0500

    add  mobile responsiveness for contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -9,10 +9,10 @@ export default function Home() {
   return (
     <>
       <Header classes="absolute" />
-      <main className="min-h-svh w-full bg-bg-black px-[12rem] pt-[8rem] text-gray-1">
-        <section className="flex h-full w-full">
+      <main className="min-h-svh w-full bg-bg-black px-[12rem] pt-[8rem] text-gray-1 portrait:px-[2rem]">
+        <section className="flex h-full w-full portrait:flex-col">
           {/* left */}
-          <div className="flex flex-1 flex-col gap-[14rem] pt-[7.4rem] pr-[7.4rem] pb-[4rem]">
+          <div className="flex flex-1 flex-col gap-[14rem] portrait:gap-[6rem] pt-[7.4rem] portrait:pt-[4rem] pr-[7.4rem] pb-[4rem]">
             <div>
               <h1 className="mb-[2.4rem] text-[6.4rem] leading-[110%] font-medium tracking-[-0.04em] text-nowrap">
                 Let&apos;s start a <br /> project together
@@ -24,7 +24,7 @@ export default function Home() {
               </p>
             </div>
 
-            <div className="flex flex-col gap-[3.2rem] text-[1.4rem] tracking-[-0.04em] text-gray-1">
+            <div className="flex flex-col gap-[3.2rem] text-[1.4rem] tracking-[-0.04em] text-gray-1 portrait:hidden">
               <ul>
                 <li className="mb-[0.2rem] text-[1.2rem] leading-[120%] text-gray-3 uppercase">CONTACT DETAILS</li>
                 <li className="">
@@ -52,8 +52,37 @@ export default function Home() {
           </div>
           {/* right */}
 
-          <div className="w-full flex-[1.1] py-[3.2rem] pl-[7.4rem]">
+          <div className="w-full flex-[1.1] py-[3.2rem] portrait:pt-0 portrait:pb-[4rem] pl-[7.4rem] portrait:pl-0">
             <CustomForm />
+          </div>
+
+          <div className="flex-wrap gap-[3.2rem] justify-between gap-x-[6.4rem] text-[1.4rem] tracking-[-0.04em] text-gray-1 hidden portrait:flex pb-[6rem]">
+            <ul className="flex flex-wrap  gap-x-[1.2rem]">
+              <li className="mb-[0.6rem] basis-[100%] text-[1.2rem] leading-[120%] text-gray-3 uppercase">SOCIALS</li>
+              {FOOTER_SOCIALS.slice(1)
+                .reverse()
+                .map(({ href, title }) => (
+                  <li key={href}>
+                    <a href={href}>{title}</a>
+                  </li>
+                ))}
+            </ul>
+
+            <ul>
+              <li className="mb-[0.2rem] text-[1.2rem] leading-[120%] text-gray-3 uppercase">CONTACT DETAILS</li>
+              <li className="">
+                <a href="mailto:shatlykabdullayev1011@gmail.com">shatlykabdullayev1011@gmail.com</a>
+              </li>
+            </ul>
+
+            <ul>
+              <li className="mb-[0.6rem] text-[1.2rem] leading-[120%] tracking-[-0.01em] text-gray-3 uppercase">
+                CURRENT LOCATION
+              </li>
+              <li>Home, Turkmenabat</li>
+            </ul>
+
+
           </div>
         </section>
         <Toaster theme="dark" position="top-center" />
