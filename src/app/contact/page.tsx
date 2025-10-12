048c4ca6870d54946839cdd51bae9091aa59bd58
Shatlyk Abdullayev <gj_wp@mail.ru>
2025-10-12 22:46:40 +0500

    prettier format

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <main className="min-h-svh w-full bg-bg-black px-[12rem] pt-[8rem] text-gray-1 portrait:px-[2rem]">
         <section className="flex h-full w-full portrait:flex-col">
           {/* left */}
-          <div className="flex flex-1 flex-col gap-[14rem] portrait:gap-[6rem] pt-[7.4rem] portrait:pt-[4rem] pr-[7.4rem] pb-[4rem]">
+          <div className="flex flex-1 flex-col gap-[14rem] pt-[7.4rem] pr-[7.4rem] pb-[4rem] portrait:gap-[6rem] portrait:pt-[4rem]">
             <div>
               <h1 className="mb-[2.4rem] text-[6.4rem] leading-[110%] font-medium tracking-[-0.04em] text-nowrap">
                 Let&apos;s start a <br /> project together
@@ -52,12 +52,12 @@ export default function Home() {
           </div>
           {/* right */}
 
-          <div className="w-full flex-[1.1] py-[3.2rem] portrait:pt-0 portrait:pb-[4rem] pl-[7.4rem] portrait:pl-0">
+          <div className="w-full flex-[1.1] py-[3.2rem] pl-[7.4rem] portrait:pt-0 portrait:pb-[4rem] portrait:pl-0">
             <CustomForm />
           </div>
 
-          <div className="flex-wrap gap-[3.2rem] justify-between gap-x-[6.4rem] text-[1.4rem] tracking-[-0.04em] text-gray-1 hidden portrait:flex pb-[6rem]">
-            <ul className="flex flex-wrap  gap-x-[1.2rem]">
+          <div className="hidden flex-wrap justify-between gap-[3.2rem] gap-x-[6.4rem] pb-[6rem] text-[1.4rem] tracking-[-0.04em] text-gray-1 portrait:flex">
+            <ul className="flex flex-wrap gap-x-[1.2rem]">
               <li className="mb-[0.6rem] basis-[100%] text-[1.2rem] leading-[120%] text-gray-3 uppercase">SOCIALS</li>
               {FOOTER_SOCIALS.slice(1)
                 .reverse()
@@ -81,8 +81,6 @@ export default function Home() {
               </li>
               <li>Home, Turkmenabat</li>
             </ul>
-
-
           </div>
         </section>
         <Toaster theme="dark" position="top-center" />
